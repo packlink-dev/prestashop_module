83a02b909a4744c8fe28396676d6ce0df3bd4515
Marija <marija.ivic@logeecom.com>
2025-02-18 10:23:50 +0100

    Update core

--- src/controllers/admin/ManualRefreshServiceController.php
+++ src/controllers/admin/ManualRefreshServiceController.php
@@ -1,9 +1,26 @@
 <?php
+/**
+ * 2025 Packlink
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Apache License 2.0
+ * that is bundled with this package in the file LICENSE.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.apache.org/licenses/LICENSE-2.0.txt
+ *
+ * @author    Packlink <support@packlink.com>
+ * @copyright 2025 Packlink Shipping S.L
+ * @license   http://www.apache.org/licenses/LICENSE-2.0.txt  Apache License 2.0
+ */
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
 
 
 /** @noinspection PhpIncludeInspection */
 
-use Packlink\BusinessLogic\Controllers\ManualRefreshServiceController as CoreController;
+use Packlink\BusinessLogic\Controllers\ManualRefreshController as CoreController;
 use Packlink\PrestaShop\Classes\Utility\PacklinkPrestaShopUtility;
 
 require_once rtrim(_PS_MODULE_DIR_, '/') . '/packlink/vendor/autoload.php';
@@ -27,7 +44,7 @@ class ManualRefreshServiceController extends PacklinkBaseController
 
     public function displayAjaxRefreshService()
     {
-        PacklinkPrestaShopUtility::dieJson($this->controller->enqueueUpdateTask());
+        PacklinkPrestaShopUtility::dieJson($this->controller->enqueueUpdateTask()->toArray());
     }
 
     /**
@@ -38,6 +55,6 @@ class ManualRefreshServiceController extends PacklinkBaseController
      */
     public function displayAjaxGetTaskStatus()
     {
-        PacklinkPrestaShopUtility::dieJson($this->controller->getTaskStatus());
+        PacklinkPrestaShopUtility::dieJson($this->controller->getTaskStatus()->toArray());
     }
 }
